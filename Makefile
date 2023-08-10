6c93ecc065378bde6754e3d83b2f266470ca9066
gary <1625721671@qq.com>
2023-08-11 01:41:23 +0800

    Add grpc connection

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 unittest:
 	PYTHONPATH=`pwd` python3 -m pytest tests --cov=pymilvus -v
 
+version:
+	python3 setup.py --version
+	
 package:
 	python3 -m build --sdist --wheel --outdir dist/ .
 
--- proto_make.sh
+++ proto_make.sh
@@ -5,6 +5,6 @@ PROTO_DIR="dingodb/protos/dingo-proto/proto"
 
 python -m pip install "grpcio-tools==$(python3 -c 'import grpc; print(grpc.__version__)')"
 
-python -m grpc_tools.protoc -I ${PROTO_DIR} --python_out=${OUTDIR} --pyi_out=${OUTDIR} ${PROTO_DIR}/*.proto
+python -m grpc_tools.protoc -I ${PROTO_DIR} --python_out=${OUTDIR} --pyi_out=${OUTDIR} --grpc_python_out=${OUTDIR}  ${PROTO_DIR}/*.proto
 
 echo "Success to generate the python proto files."
